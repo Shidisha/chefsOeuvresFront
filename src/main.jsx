08c004db851eab9c7463ff220640370b4059a140
djoKadima <djokadimatshibangu@gmail.com>
2024-05-10 18:32:50 +0100

    update route

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import Home from './pages/public/home.jsx'
@@ -19,11 +18,7 @@ import SignupPage from './pages/public/auth/signupPage.jsx'
 
 const router = createBrowserRouter([
   
-  {
-    path: '/',
-    element: <App/>,
-    children: [
-      {
+        {
         path: '/',
         element: <Home/>, 
         children : [
@@ -64,25 +59,24 @@ const router = createBrowserRouter([
       {
         path: '*',
         element: <NotFound/>
-      }
-      
-    ]
-  },
-  {
-    path: '/',
-    element: <Authentification/>,
-    children: [
-      {
-        path:'/loginPage',
-        element:<LoginPage/>,
       },
       {
-        path:'/signupPage',
-        element:<SignupPage/>,
-      }
+        path: '/',
+        element: <Authentification/>,
+        children: [
+          {
+            path:'/loginPage',
+            element:<LoginPage/>,
+          },
+          {
+            path:'/signupPage',
+            element:<SignupPage/>,
+          }
+        ]
+    },
+      
     ]
-},
-])
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router}/>
