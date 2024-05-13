52a3d9bdda823e681a1c6f30f1d50d62b7863fcc
djoKadima <djokadimatshibangu@gmail.com>
2024-05-13 18:14:39 +0100

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
-import Header from './components/header'
 
 function App() {
   return (
     <div>
-      <Header/>
       <Outlet/> 
     </div>
   )
