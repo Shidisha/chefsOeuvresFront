6b10e675b48f691528b25c8a4c0ea825ff07d113
djoKadima <djokadimatshibangu@gmail.com>
2024-05-16 10:27:51 +0100

    fix

--- src/components/button_search.jsx
+++ src/components/button_search.jsx
@@ -1,7 +1,7 @@
 const ButtonSearch = () =>{
     return(
         <>
-            <button className="border border-black rounded-full px-1 py-2">RECHERCHER</button>
+            <button className="border border-black rounded-full px-1 py-2 bg-red-400 text-white">RECHERCHER</button>
         </>
     )
 }
